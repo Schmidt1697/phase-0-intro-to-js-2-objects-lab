756865baa896c096886feb6b3f076001f3c5fa01
Anna Schmidt <marshall1697@gmail.com>
2022-08-31 10:50:54 -0600

    commit tree 7fb8bbfdb7cf26ab1285478a992fabc869db325c

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee ={};
+
+//non destructive
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee}
+    newEmployee[key] = value;
+    return newEmployee;
+    
+};
+
+//destructive
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value; 
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee}
+    delete newEmployee[key];
+    return newEmployee;
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
